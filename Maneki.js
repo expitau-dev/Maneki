1a6b17cb5adc054d5fb0752629e5a8a6880905e7
expitau-dev <expitau@gmail.com>
2021-10-28 17:38:22 -0400

    Updated to use event listeners and be more encapsulated

--- Maneki.js
+++ Maneki.js
@@ -1,132 +1,197 @@
-class Maneki_Terminal {
-    constructor(id) {
-        this.id = "m_terminal_" + id;
+// Append a stylesheet to <head> to style terminals
+document.addEventListener("DOMContentLoaded", () => {
+    let stylesheet = document.createElement('style');
+    stylesheet.innerHTML = `
+        .m_terminal, .m_terminal * {
+            font-family: monospace; 
+            margin: 0; 
+            padding: 0px;
+        }
+        .m_terminal {
+            height: 100%;
+            overflow-y: scroll;
+        }
+        .m_terminal .m_input {
+            border: none;
+            outline: none;
+            background: none;
+            color: inherit;
+            font-size: initial;
+            flex: 1;
+        }
+        .m_terminal .m_prompt { 
+            display: flex;
+        } 
+        .m_terminal .m_context { 
+            display: inline; 
+        } 
+
+        .m_terminal::-webkit-scrollbar { 
+            width: 15px; 
+        } 
+        .m_terminal::-webkit-scrollbar-track { 
+            background: none; 
+        } 
+        .m_terminal::-webkit-scrollbar-thumb { 
+            background: rgb(50, 50, 50); 
+        } 
+        .m_terminal::-webkit-scrollbar-thumb:hover { 
+            background: rgb(70, 70, 70);
+        }
+    `;
+    document.head.appendChild(stylesheet)
+})
+
+// Define library
+let Maneki = new (function () {
+    // For unique terminal ids
+    let terminalIndex = 0;
+
+    function Terminal(selector) {
+        // this.id = 0, 1, 2...
+        this.id = terminalIndex++;
+
+        // Displayed before input field
+        this.context = ">>>_";
+
+        // The content to write, either a string or an html element
         this.content = [];
-        this.context = ">>> ";
-        this.commands = [];
-    }
 
-    attachCommand(trigger, f) {
-        this.commands.push({ rexp: trigger, action: f })
-    }
+        // HTML id of terminal
+        this.selector = selector;
 
-    onKeyDown() {
-        var event = window.event || event.which;
+        // Has the DOM been initialized yet
+        this.initialized = false;
 
-        if (event.keyCode == 13) {
-            event.preventDefault();
-            var line = document.getElementById(this.id + "_input").value
-            this.write(this.context + line);
-            this.commands.forEach(cmd => line.match(cmd.rexp) && cmd.action(line))
-            document.getElementById(this.id + "_input").value = "";
+        // List of event listeners
+        let _eventlisteners = {};
+
+        // Append to _eventlisteners
+        this.addEventListener = (e, f) => {
+            _eventlisteners[e] || (() => {_eventlisteners[e] = []})();
+            _eventlisteners[e].push(f)
         }
 
-        document.getElementById(this.id + "_input").style.height = (document.getElementById(this.id + "_input").scrollHeight) + "px";
-    }
-
-    flush() {
-        let text = document.getElementById(this.id + "_text");
-        text.innerHTML = '';
-        this.content.forEach((line) => {
-            text.appendChild(document.createTextNode(line))
-            text.appendChild(document.createElement("br"))
-        })
-        document.getElementById(this.id + "_context").innerHTML = this.context;
-    }
-
-    write(line = "") {
-        // img = line.split("image/")[1]
-        // imageElement = document.createElement("img")
-        // imageElement.setAttribute("src", img)
-        // imageElement.setAttribute("width", "42%")
-        // document.getElementById("m_terminal_0_text").appendChild(imageElement);
-        // document.getElementById("m_terminal_0_text").appendChild(document.createElement("br"));
-        // document.getElementById("m_terminal_0_text").appendChild(document.createElement("br"));
-        for (const part of line.split("\n")) {
-            this.content.push(part);
-        }
-        this.flush()
-        document.getElementById(this.id).scrollTop = document.getElementById(this.id).scrollHeight;
-    }
-
-    clear() {
-        this.content = []
-        this.flush()
-    }
-}
-
-class Maneki {
-
-    constructor(n = 0) {
-        if (n == 0) {
-            this.terminals = Array.from(document.getElementsByTagName('terminal')).map((x, i) => (new Maneki_Terminal(i)))
-            this._init();
-        } else {
-            this.terminals = Array(n).fill().map((x, i) => (new Maneki_Terminal(i)))
-            document.addEventListener("DOMContentLoaded", () => {
-                this._init();
-            })
+        // Remove from _eventlisteners
+        this.removeEventListener = (e, f) => {
+            _eventlisteners[e] = _eventlisteners[e].filter(i => {return i != f})
         }
 
+        // Execute all listening events
+        this.dispatchEvent = (e, ...args) => {
+            for (const f of (_eventlisteners[e] || [])){
+                f(...args);
+                this.removeEventListener(e,f)
+            }
+        }
+
+        // When the user enters a key in the input field
+        this.onKeyDown = () => {
+            // Get the event
+            let event = window.event || event.which;
+            if (event.keyCode == 13) { // If the user presses the 'enter' key
+                event.preventDefault();
+
+                // Get the line the user wrote
+                let inputElement = document.querySelector(`#m_terminal_${this.id} .m_prompt .m_input`)
+                let line = inputElement.value
+
+                // Print the line the user wrote
+                this.write(this.context + line);
+                inputElement.value = "";
+
+                // Execute all listening commands
+                this.dispatchEvent("onCommand", line)
+            }
+        }
+
+        // Flush this.content to the screen
+        this.flush = () => {
+            if (this.initialized) {
+                let text = document.querySelector(`#m_terminal_${this.id} .m_text`);
+                text.innerHTML = '';
+                this.content.forEach((line) => {
+                    let element;
+                    if (typeof line === 'string') {
+                        element = document.createElement("p")
+                        element.appendChild(document.createTextNode(line))
+                    } else if (typeof line === 'object') {
+                        element = line;
+                    }
+                    text.appendChild(element)
+                })
+                document.querySelector(`#m_terminal_${this.id} .m_prompt .m_context`).innerHTML = this.context;
+                document.querySelector(`#m_terminal_${this.id}`).scrollTop = document.querySelector(`#m_terminal_${this.id}`).scrollHeight;
+            } else {
+                document.addEventListener("DOMContentLoaded", () => { this.initialized && this.flush() })
+            }
+        }
+
+        // Append a line to the terminal
+        this.write = (line = "") => {
+            // img = line.split("image/")[1]
+            // imageElement = document.createElement("img")
+            // imageElement.setAttribute("src", img)
+            // imageElement.setAttribute("width", "42%")
+            // document.getElementById("m_terminal_0_text").appendChild(imageElement);
+            // document.getElementById("m_terminal_0_text").appendChild(document.createElement("br"));
+            // document.getElementById("m_terminal_0_text").appendChild(document.createElement("br"));
+            if (typeof line === 'string') {
+                for (const part of line.split("\n")) {
+                    this.content.push(part);
+                }
+            } else {
+                this.content.push(line);
+            }
+            this.flush()
+        }
+
+        // Clear the terminal
+        this.clear = () => {
+            this.content = []
+            this.flush()
+        }
+
+        // Initialize the terminal display. Only execute after 'DOMContentLoaded'
+        this.init = () => {
+            let element = document.querySelector("terminal#" + this.selector)
+            if (!element)
+                return null
+
+            element.outerHTML = `
+            <div style="${element.style?.cssText}" id="${element.id || ''}" class="${element.class || ''}">
+                <div style="all:initial; background-color: inherit; color: inherit"> 
+                    <div class="m_terminal" id="m_terminal_${this.id}" onClick="document.querySelector('#m_terminal_${this.id} .m_prompt .m_input').focus()"> 
+                        <p class="m_text"></p> 
+                        <div class="m_prompt"> 
+                            <p class="m_context" style="white-space: pre;"></p> 
+                            <input class="m_input" autofocus rows="1" onkeydown="Maneki.getTerminal('${this.selector}').onKeyDown();"></input> 
+                        </div> 
+                    </div> 
+                </div> 
+            </div>`;
+            this.initialized = true;
+            this.flush();
+            return element;
+        }
+
+        // Initialize, or wait to initialize
+        this.element = this.init();
+        this.element || document.addEventListener("DOMContentLoaded", () => { this.element = this.init() })
+    };
+
+    // List of terminals
+    let terminals = {}
+    this.getTerminal = (id) => {
+        if (!terminals[id])
+            terminals[id] = new Terminal(id)
+        return terminals[id]
     }
+})();
 
-    _init() {
-        let terminal_stylesheet = document.createElement('style')
-        terminal_stylesheet.innerHTML = " \
-        .m_terminal { \
-            margin: 0; \
-            padding: 0px; \
-            height: 100%; \
-            overflow-y: auto; \
-            font-family: Monospace; \
-        } \
-        .m_terminal_text { \
-            margin: 0px 0px 0px 0px; \
-        } \
-        .m_terminal_input { \
-            resize: none; \
-            margin: -10px 0px -3px -9px; \
-            border: none; \
-            outline: none; \
-            background: none; \
-            width: 90%; \
-            overflow: hidden; \
-            color: inherit; \
-        } \
-        .m_terminal_prompt { \
-            margin: 0px 0px 0px 0px; \
-        } \
-        .m_terminal_context { \
-            display: inline; \
-        } \
-        .m_terminal::-webkit-scrollbar { \
-            width: 15px; \
-        } \
-        .m_terminal::-webkit-scrollbar-track { \
-            background: rgb(0, 0, 0); \
-        } \
-        .m_terminal::-webkit-scrollbar-thumb { \
-            background: rgb(50, 50, 50); \
-        } \
-        .m_terminal::-webkit-scrollbar-thumb:hover { \
-            background: rgb(70, 70, 70); \
-        }"
-        document.head.appendChild(terminal_stylesheet);
-
-        Array.from(document.getElementsByTagName('terminal')).forEach((element, i) => {
-            element.outerHTML = " \
-            <div style=\"" + element.style.cssText + "\"" + " " + `id=\"${element.id || ''}\"` + " " + `class=\"${element.class || ''}\"` + "> \
-                <div style=\"all:initial; background-color: inherit; color: inherit\"> \
-                    <div class=\"m_terminal\" id=\"m_terminal_"+ i + "\" onClick=\"document.getElementById('m_terminal_" + i + "_input').focus()\"> \
-                        <p class=\"m_terminal_text\" id=\"m_terminal_"+ i + "_text\"></p> \
-                        <div class=\"m_terminal_prompt\" id=\"m_terminal_"+ i + "_prompt\"> \
-                            <p style=\"white-space: pre;\" class=\"m_terminal_context\" id=\"m_terminal_" + i + "_context\">" + this.terminals[i].context + "</p> \
-                            <textarea class=\"m_terminal_input\" autofocus rows=\"1\" id=\"m_terminal_"+ i + "_input\" onkeydown=\"maneki.terminals[" + i + "].onKeyDown();\"></textarea> \
-                        </div> \
-                    </div> \
-                </div> \
-            </div>";
-        });
+// Initialize all terminals
+document.addEventListener("DOMContentLoaded", () => {
+    for (const e of document.querySelectorAll("terminal")) {
+        Maneki.getTerminal(`${e.id}`);
     }
-}
-
+})
